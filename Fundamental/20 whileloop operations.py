c6ee88935ecd6def554c77cca0aa6ab4db9fe11e
shrutisinhaa <shruti.sinha@hotmail.com>
2026-02-24 22:46:21 +0530

    20,21,

---
+++ Fundamental/20 whileloop operations.py
@@ -0,0 +1,24 @@
+num=10
+i=1
+sum=0
+
+while i<=num:
+    sum=sum+i
+    i=i+1
+    print(sum)
+else:
+    print("No more number in list")
+print(sum) ## 55
+##################   result
+#1
+#3
+#6
+#10
+#15
+#21
+#28
+#36
+#45
+#55
+#No more number in list
+#55
---
+++ Fundamental/21 Break statement.py
@@ -0,0 +1,15 @@
+# break statement break the loop
+# continue skips loop for current iteration and goto next. but doesn't break the loop
+
+for i in "abcdef":
+    if i == "d":
+        break
+    print(i)
+## break the loop once i reaches d hence result is
+# a
+# b
+# c
+
+#######################################
+
+# you can also create if-else statement similarly
